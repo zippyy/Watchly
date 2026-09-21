cc95e28c825f19aae369d8e0bffacb4d16909c91
Zippy <zippyy@zoho.com>
2026-09-21 16:48:43 -0600

    Use standard manifest path for Nuvio mode

--- app/api/endpoints/manifest.py
+++ app/api/endpoints/manifest.py
@@ -19,7 +19,7 @@ async def manifest_token(token: str):
     return await manifest_service.get_manifest_for_token(token)
 
 
-@router.get("/{token}/nuvio-manifest.json")
+@router.get("/{token}/nuvio/manifest.json")
 async def nuvio_manifest_token(token: str):
     manifest = await manifest_service.get_manifest_for_token(token)
     return build_nuvio_manifest(manifest)
